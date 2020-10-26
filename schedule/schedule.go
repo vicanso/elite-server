5b80f23f0b01f97b72b2ed47440e950f1824a6f7
vicanso <vicansocanbico@gmail.com>
2020-10-26 20:31:50 +0800

    refactor: adjust schedule

--- schedule/schedule.go
+++ schedule/schedule.go
@@ -28,17 +28,22 @@ import (
 	"go.uber.org/zap"
 )
 
+type (
+	taskFn      func() error
+	statsTaskFn func() map[string]interface{}
+)
+
 var (
 	logger = log.Default()
 )
 
 func init() {
 	c := cron.New()
-	_, _ = c.AddFunc("@every 5m", redisCheck)
-	_, _ = c.AddFunc("@every 5m", entCheck)
+	_, _ = c.AddFunc("@every 1m", redisPing)
+	_, _ = c.AddFunc("@every 1m", entPing)
 	_, _ = c.AddFunc("@every 1m", configRefresh)
 	_, _ = c.AddFunc("@every 5m", redisStats)
-	_, _ = c.AddFunc("@every 10s", entStats)
+	_, _ = c.AddFunc("@every 5m", entStats)
 	_, _ = c.AddFunc("@every 30s", cpuUsageStats)
 	_, _ = c.AddFunc("@every 1m", performanceStats)
 	_, _ = c.AddFunc("@every 24h", updateAllNovelWordCount)
@@ -57,73 +62,75 @@ func init() {
 	c.Start()
 }
 
-func redisCheck() {
-	err := helper.RedisPing()
+func doTask(desc string, fn func() error) {
+	startedAt := time.Now()
+	err := fn()
 	if err != nil {
-		logger.Error("redis check fail",
+		logger.Error(desc+" fail",
+			zap.String("category", "schedule"),
+			zap.Duration("use", time.Since(startedAt)),
 			zap.Error(err),
 		)
-		service.AlarmError("redis check fail, " + err.Error())
+		service.AlarmError(desc + " fail, " + err.Error())
+	} else {
+		logger.Info(desc+" success",
+			zap.String("category", "schedule"),
+			zap.Duration("use", time.Since(startedAt)),
+		)
 	}
+}
+
+func doStatsTask(desc string, fn statsTaskFn) {
+	startedAt := time.Now()
+	stats := fn()
+	logger.Info(desc,
+		zap.String("category", "schedule"),
+		zap.Duration("use", time.Since(startedAt)),
+		zap.Any("stats", stats),
+	)
+}
+
+func redisPing() {
+	doTask("redis ping", helper.RedisPing)
 }
 
 func configRefresh() {
 	configSrv := new(service.ConfigurationSrv)
-	err := configSrv.Refresh()
-	if err != nil {
-		logger.Error("config refresh fail",
-			zap.Error(err),
-		)
-		service.AlarmError("config refresh fail, " + err.Error())
-	}
+	doTask("config refresh", configSrv.Refresh)
 }
 
 func redisStats() {
-	// 统计中除了redis数据库的统计，还有当前实例的统计指标，因此所有实例都会写入统计
-	stats := helper.RedisStats()
-	helper.GetInfluxSrv().Write(cs.MeasurementRedisStats, stats, nil)
+	doStatsTask("redis stats", func() map[string]interface{} {
+		// 统计中除了redis数据库的统计，还有当前实例的统计指标，因此所有实例都会写入统计
+		stats := helper.RedisStats()
+		helper.GetInfluxSrv().Write(cs.MeasurementRedisStats, stats, nil)
+		return stats
+	})
 }
 
-func entCheck() {
-	err := helper.EntPing()
-	if err != nil {
-		logger.Error("ent check fail",
-			zap.Error(err),
-		)
-		service.AlarmError("ent check fail, " + err.Error())
-	}
+func entPing() {
+	doTask("ent ping", helper.EntPing)
 }
 
 // entStats ent的性能统计
 func entStats() {
-	stats := helper.EntGetStats()
-	helper.GetInfluxSrv().Write(cs.MeasurementEntStats, stats, nil)
+	doStatsTask("ent stats", func() map[string]interface{} {
+		stats := helper.EntGetStats()
+		helper.GetInfluxSrv().Write(cs.MeasurementEntStats, stats, nil)
+		return stats
+	})
 }
 
 // syncNovelSource 同步小说源
 func syncNovelSource() {
 	srv := novel.Srv{}
-	err := srv.SyncSource()
-	if err != nil {
-		logger.Error("sync novel source fail",
-			zap.Error(err),
-		)
-	} else {
-		logger.Info("sync novel source done")
-	}
+	doTask("sync novel source", srv.SyncSource)
 }
 
 // updateAllNovelWordCount 更新小说总字数
 func updateAllNovelWordCount() {
 	srv := novel.Srv{}
-	err := srv.UpdateAllWordCount()
-	if err != nil {
-		logger.Error("update all novel word count fail",
-			zap.Error(err),
-		)
-	} else {
-		logger.Info("update all novel word count done")
-	}
+	doTask("update all novel word count", srv.UpdateAllWordCount)
 }
 
 // newUpdateNovelChapterByWeight 创建更新小说章节任务
@@ -131,42 +138,24 @@ func newUpdateNovelChapterByWeight(updatedWeight int) func() {
 	return func() {
 		// 暂时不更新，等所有小说同步完成后再启用
 		// srv := novel.Srv{}
-		// err := srv.UpdateAllChaptersByWeight(updatedWeight)
-		// if err != nil {
-		// 	logger.Error("update novel chaptesr by weight fail",
-		// 		zap.Error(err),
-		// 	)
-		// } else {
-		// 	logger.Info("update novel chapters by weight success")
-		// }
+		// doTask("update novel chapters by weight", func() error {
+		// 	return srv.UpdateAllChaptersByWeight(updatedWeight)
+		// })
 	}
 }
 
 // updateAllNovelUpdatedWeight 更新小说更新权重
 func updateAllNovelUpdatedWeight() {
 	srv := novel.Srv{}
-	err := srv.UpdateAllUpdatedWeight()
-	if err != nil {
-		logger.Error("update novel updated weight fail",
-			zap.Error(err),
-		)
-	} else {
-		logger.Info("update all novel updated weight")
-	}
+	doTask("update novel updated weight", srv.UpdateAllUpdatedWeight)
 }
 
 // cpuUsageStats cpu使用率
 func cpuUsageStats() {
-	err := service.UpdateCPUUsage()
-	if err != nil {
-		logger.Error("update cpu usage fail",
-			zap.Error(err),
-		)
-		service.AlarmError("update cpu usage fail, " + err.Error())
-	}
+	doTask("update cpu usage", service.UpdateCPUUsage)
 }
 
-// prevMemFrees 上一次 memory 释放的次数
+// prevMemFrees 上一次 memory objects 释放的数量
 var prevMemFrees uint64
 
 // prevNumGC 上一次 gc 的次数
@@ -177,21 +166,24 @@ var prevPauseTotal time.Duration
 
 // performanceStats 系统性能
 func performanceStats() {
-	data := service.GetPerformance()
-	fields := map[string]interface{}{
-		"goMaxProcs":   data.GoMaxProcs,
-		"concurrency":  data.Concurrency,
-		"memSys":       data.MemSys,
-		"memHeapSys":   data.MemHeapSys,
-		"memHeapInuse": data.MemHeapInuse,
-		"memFrees":     data.MemFrees - prevMemFrees,
-		"routineCount": data.RoutineCount,
-		"cpuUsage":     data.CPUUsage,
-		"numGC":        data.NumGC - prevNumGC,
-		"pause":        (data.PauseTotalNs - prevPauseTotal).Milliseconds(),
-	}
-	prevMemFrees = data.MemFrees
-	prevNumGC = data.NumGC
+	doStatsTask("performance stats", func() map[string]interface{} {
+		data := service.GetPerformance()
+		fields := map[string]interface{}{
+			"goMaxProcs":   data.GoMaxProcs,
+			"concurrency":  data.Concurrency,
+			"memSys":       data.MemSys,
+			"memHeapSys":   data.MemHeapSys,
+			"memHeapInuse": data.MemHeapInuse,
+			"memFrees":     data.MemFrees - prevMemFrees,
+			"routineCount": data.RoutineCount,
+			"cpuUsage":     data.CPUUsage,
+			"numGC":        data.NumGC - prevNumGC,
+			"pause":        (data.PauseTotalNs - prevPauseTotal).Milliseconds(),
+		}
+		prevMemFrees = data.MemFrees
+		prevNumGC = data.NumGC
 
-	helper.GetInfluxSrv().Write(cs.MeasurementPerformance, fields, nil)
+		helper.GetInfluxSrv().Write(cs.MeasurementPerformance, fields, nil)
+		return fields
+	})
 }
