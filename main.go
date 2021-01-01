e98593dd36aab346f711a5c9632bfb68a2f19906
vicanso <vicansocanbico@gmail.com>
2021-01-01 09:56:10 +0800

    refactor: enhance performance stats

--- main.go
+++ main.go
@@ -17,8 +17,6 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"net"
-	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
@@ -177,20 +175,7 @@ func newOnErrorHandler(e *elton.Elton) {
 
 func main() {
 	e := elton.New()
-	e.Server.ConnState = func(c net.Conn, cs http.ConnState) {
-		switch cs {
-		case http.StateNew:
-			service.IncreaseConnAlive()
-		case http.StateActive:
-			service.IncreaseConnConcurrency()
-		case http.StateIdle:
-			service.DecreaseConnConcurrency()
-		case http.StateHijacked:
-			fallthrough
-		case http.StateClosed:
-			service.DecreaseConnAlive()
-		}
-	}
+	e.Server.ConnState = service.GetHTTPServerConnState()
 
 	logger := log.Default()
 	defer func() {
